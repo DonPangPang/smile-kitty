12c8718f8229d53cef4d0b74243e37f05ec8ad0c
Powers <powertle@outlook.com>
2023-12-18 17:44:54 +0800

    init

---
+++ src/SmileKitty.Domain/Logs/LoginLog.cs
@@ -0,0 +1,16 @@
+﻿using SmileKitty.Domain.Users;
+using SmileKitty.Infrastructure.Entity;
+
+namespace SmileKitty.Domain.Logs;
+
+public class LoginLog : AggregateRootBase, IEntity, ICreation
+{
+    public Guid UserAuthorizationId { get; private set; }
+    public UserAuthorization? UserAuthorization { get; private set; }
+
+    public required string Ip { get; set; }
+    public required string IpAddress { get; set; }
+    public required string UserAgent { get; set; }
+
+    public DateTime CreateTime { get; private set; }
+}
